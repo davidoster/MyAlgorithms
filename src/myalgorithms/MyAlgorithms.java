925bc73ff37af1d1a8ee35a50511be62959f303f
David Oster aka George Pasparakis <paspa@hotmail.com>
2019-06-21 20:27:46 +0300

    Update MyAlgorithms.java

--- src/myalgorithms/MyAlgorithms.java
+++ src/myalgorithms/MyAlgorithms.java
@@ -20,6 +20,7 @@ public class MyAlgorithms {
     public static void main(String[] args) {
         // TODO code application logic here
         List<String> strings = new ArrayList<>();
+        List<String> strings2 = new ArrayList<>();
     }
     
 }
